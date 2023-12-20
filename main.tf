6618b9c4b8fc630579884ced3a323a9031248541
Poorya Bagheri <puriabagheri@gmail.com>
2023-12-20 15:31:24 +0800

    create new dev vpc

--- main.tf
+++ main.tf
@@ -18,15 +18,43 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2v"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [aws_security_group.blog.id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog_new"
+
+  vpc_id = module.blog_vpc.vpc_id
+  ingress_rules = ["http-80-tcp", "https-443]
 }
 
 resource "aws_security_group" "blog" {
